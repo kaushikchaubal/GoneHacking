898038c5534193506065e17c77b07a3c7da7393e
LabsterX <labsterx@gmail.com>
2015-05-16 15:16:06 +0100

    added templates for header and footer

--- app/main/main.controller.js
+++ app/main/main.controller.js
@@ -1,8 +1,9 @@
 'use strict';
 
 angular.module('GoInvestApp')
-	.controller('MainCtrl', function ($http, $scope) {
-	  var ref = new Firebase("https://gonehacking-goinvestx.firebaseio.com/");
+.controller('MainCtrl', function ($http, $scope, $firebaseObject) {
+// .controller('MainCtrl', function ($http, $scope, 'firebase') {
+	  var ref = new Firebase("https://gonehacking-goinvest.firebaseio.com/");
 
 	  // download the data into a local object
 	  var syncObject = $firebaseObject(ref);
