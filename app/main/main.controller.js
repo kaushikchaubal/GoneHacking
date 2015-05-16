6bfcdb9d7d3710e6ea30e422fd684e6adef46217
Dhilip Buddha <dhilipb@gmail.com>
2015-05-16 14:53:47 +0100

    Firebase shizzle

--- app/main/main.controller.js
+++ app/main/main.controller.js
@@ -2,5 +2,12 @@
 
 angular.module('GoInvestApp')
 	.controller('MainCtrl', function ($http, $scope) {
+	  var ref = new Firebase("https://gonehacking-goinvestx.firebaseio.com/");
+
+	  // download the data into a local object
+	  var syncObject = $firebaseObject(ref);
+
+	  // putting a console.log here won't work, see below
+	  syncObject.$bindTo($scope, "data");
 
 	});
