22af5b4c33afada5ed3586333de136878ff0007c
Dhilip Buddha <dhilipb@Dhilips-MacBook-Pro.local>
2015-05-16 13:57:15 +0100

    Fixing all startup bugs

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-var app = angular.module("GoInvestApp", ["firebase"]);
+var app = angular.module("GoInvestApp", ["firebase"])
 
-app.controller("MainCtrl", function($scope, $firebaseObject) {
-  var ref = new Firebase("https://gonehacking-goinvest.firebaseio.com/");
+.controller("MainCtrl", function($scope, $firebaseObject) {
+  var ref = new Firebase("https://gonehacking-goinvestx.firebaseio.com/");
 
   // download the data into a local object
-  $scope.data = $firebaseObject(ref);
+  var syncObject = $firebaseObject(ref);
 
   // putting a console.log here won't work, see below
   syncObject.$bindTo($scope, "data");
