8f8deb6725f2a4fde3ba2af6b50e23c278e7cc85
LabsterX <labsterx@gmail.com>
2015-05-17 14:45:17 +0100

    tweaking

--- app/diy/diy.controller.js
+++ app/diy/diy.controller.js
@@ -32,12 +32,21 @@ angular.module('GoInvestApp')
 
 		];
 
+		$scope.selectTotal = 0;
+
+ 		$scope.riskData = [
+ 				['Equities', 100]
+ 		]
+
+		$scope.riskData = [];
+
 		$scope.addAsset = function(asset) {
 			for (var a in $scope.assets) {
 				if ($scope.assets[a] == asset) {
 					$scope.assets.splice(a, 1);
 					$scope.assets_added.push(asset);
-					$scope.apply();
+					$scope.selectTotal++;
+					// $scope.$apply();
 				}
 			}
  		};
@@ -46,9 +55,47 @@ angular.module('GoInvestApp')
 				if ($scope.assets_added[a] == asset) {
 					$scope.assets_added.splice(a, 1);
 					$scope.assets.push(asset);
-					$scope.apply();
+					$scope.selectTotal--;
+					// $scope.apply();
 				}
 			}
+ 		};
+
+
+ 		$scope.riskSelection = {
+ 			'Equities': 0,
+ 			'Alternative': 0,
+ 			'Bonds': 0,
+ 			'Cash': 0
  		}
+
+		$scope.chartConfig = {
+				chart: {
+									plotBackgroundColor: null,
+									plotBorderWidth: null,
+									plotShadow: false
+							},
+							title: "Assets",
+							tooltip: {
+									pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+							},
+							plotOptions: {
+									pie: {
+											allowPointSelect: true,
+											cursor: 'pointer',
+											dataLabels: {
+													enabled: false
+											},
+											showInLegend: true
+									}
+							},
+							series: [{
+									type: 'pie',
+									name: 'Assets',
+									data: [
+							 				['Equities', 100]
+							 		]
+							}]
+			};
 
 	})
--- app/explore/explore.controller.js
+++ app/explore/explore.controller.js
@@ -9,26 +9,31 @@ angular.module('GoInvestApp')
 			'name': 'JPM Japan C GBP Hdg Net Acc',
 			'user': 'Neil Woodford',
 			'return': 2.45,
+			'selected': false,
 		},
 		{
 			'name': 'Old Mutual UK Mid Cap R Acc',
 			'user': 'Sheldon Cooper',
 			'return': -1.02,
+			'selected': false,
 		},
 		{
 			'name': 'Schroder European Alpha Plus Inc',
 			'user': 'Joey Tribbiani',
 			'return': 2.45,
+			'selected': false,
 		},
 		{
 			'name': 'Baillie Gifford Global Discovery Inc',
 			'user': 'Ross Geller',
 			'return': 2.45,
+			'selected': false,
 		},
 		{
 			'name': 'Baillie Gifford Global Discovery Inc',
 			'user': 'Leonard Hofstader',
 			'return': 2.45,
+			'selected': false,
 		},
 		]
 	});
