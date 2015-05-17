3e787f43a1febc3419b4103bc538cd3ef0a21efd
Dhilip Buddha <dhilipb@gmail.com>
2015-05-17 14:57:04 +0100

    Numbers

--- app/explore/explore.controller.js
+++ app/explore/explore.controller.js
@@ -14,25 +14,25 @@ angular.module('GoInvestApp')
 		{
 			'name': 'Old Mutual UK Mid Cap R Acc',
 			'user': 'Sheldon Cooper',
-			'return': -1.02,
+			'return': 2.02,
 			'selected': false,
 		},
 		{
 			'name': 'Schroder European Alpha Plus Inc',
 			'user': 'Joey Tribbiani',
-			'return': 2.45,
+			'return': 1.55,
 			'selected': false,
 		},
 		{
 			'name': 'Baillie Gifford Global Discovery Inc',
 			'user': 'Ross Geller',
-			'return': 2.45,
+			'return': -0.31,
 			'selected': false,
 		},
 		{
 			'name': 'Baillie Gifford Global Discovery Inc',
 			'user': 'Leonard Hofstader',
-			'return': 2.45,
+			'return': -1.22,
 			'selected': false,
 		},
 		]
